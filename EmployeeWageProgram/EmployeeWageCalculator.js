1647aa9938dc39ed3f8cf69a89fea7674c77304f
lokesh <kottelokesh789@gmail.com>
2022-02-01 20:05:47 +0530

    ADDED: StoringDailyWageAlongWithTotalWage

--- EmployeeWageProgram/EmployeeWageCalculator.js
+++ EmployeeWageProgram/EmployeeWageCalculator.js
@@ -18,13 +18,20 @@ function getWorkingHours(empCheck){
             return 0;
     }
 }
+function calcDailyWage(empHrs){
+    return empHrs * WAGE_PER_HOUR;
+}
 //UC_5//
 let totalEmpHrs = 0;
 let totalWorkingDays = 0;
+let empDailyWageArr = new Array();
 while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
     totalWorkingDays++;
     let empCheck = Math.floor(Math.random() *10) % 3;
-    totalEmpHrs += getWorkingHours(empCheck);
+    let empHrs = getWorkingHours(empCheck);
+    totalEmpHrs += empHrs;
+    empDailyWageArr.push(calcDailyWage(empHrs));
 }
-let empWage = totalEmpHrs * WAGE_PER_HOUR;
+//UC_6//
+let empWage = calcDailyWage(totalEmpHrs);
 console.log( "TotalDays: " + totalWorkingDays+ "Total Hrs: " + totalEmpHrs + "Emp Wage: " +empWage);
