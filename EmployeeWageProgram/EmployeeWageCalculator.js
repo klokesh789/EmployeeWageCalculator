d8c43b7fb7b4cd4a1c62f1a87eb428bb41e49a0f
lokesh <kottelokesh789@gmail.com>
2022-02-01 19:35:47 +0530

    ADDED: CalculatingMonthlyWages

--- EmployeeWageProgram/EmployeeWageCalculator.js
+++ EmployeeWageProgram/EmployeeWageCalculator.js
@@ -4,6 +4,7 @@ const IS_FULL_TIME=2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
+const NUM_OF_WORKING_DAYS = 20;
 
 //UC_3//
 function getWorkingHours(empCheck){
@@ -16,8 +17,11 @@ function getWorkingHours(empCheck){
             return 0;
     }
 }
+//UC_4//
 let empHrs = 0;
-let empCheck = Math.floor(Math.random() * 10) % 3;
-empHrs = getWorkingHours(empCheck);
+for(let day = 0; day < NUM_OF_WORKING_DAYS; day++){
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    empHrs += getWorkingHours(empCheck);
+}
 let empWage = empHrs * WAGE_PER_HOUR;
-console.log("Emp Wage: " +empWage);
+console.log("Total Hrs: " + empHrs + "Emp Wage: " +empWage);
